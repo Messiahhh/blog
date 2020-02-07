523a9c6ac42c7565904b9b46acb329200cb11632
messiahhh <616348105@qq.com>
2020-02-08 03:54:32 +0800

    最终测试

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:messiahhh/blog.git master:gh-pages
-git push -f https://${access_token}github.com/messiahhh/blog.git master:gh-pages
+git push -f https://${access_token}@github.com/messiahhh/blog.git master:gh-pages
 
 
 cd -
