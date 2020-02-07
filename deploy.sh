fc7c04feee22c0f2d92587c43360b0d58264d8ed
messiahhh <616348105@qq.com>
2020-02-08 03:24:53 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:messiahhh/messiahhh.github.io.git master
+# git push -f git@github.com:messiahhh/messiahhh.github.io.git master
+git push -f https://${access_token}@github.com/messiahhh/messiahhh.github.io.git master
+
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
