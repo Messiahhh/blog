c316d4490d5efc191d86974b1bfae7e9dcfbd765
messiahhh <616348105@qq.com>
2020-10-12 16:25:58 +0800

    test

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -1,29 +1,28 @@
 <template>
-  <div class="theme-container">
-    <div class="theme-default-content">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
+    <div class="theme-container">
+        <div class="theme-default-content">
+            <h1>404</h1>
+            <blockquote>{{ getMsg() }}</blockquote>
 
-      <RouterLink to="/">
+            <!-- <RouterLink to="/">
         Take me home.
-      </RouterLink>
-
+      </RouterLink> -->
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 const msgs = [
-  `你似乎是来找Akara的？`,
-  `Akara是谁？`,
-  `我是谁？你又是谁`,
-  `下次再来吧，我困了`
+    `你似乎是来找Akara的？`,
+    `Akara是谁？`,
+    `我是谁？你又是谁`,
+    `下次再来吧，我困了`,
 ]
 export default {
-  methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
-  }
+    methods: {
+        getMsg() {
+            return msgs[Math.floor(Math.random() * msgs.length)]
+        },
+    },
 }
 </script>
