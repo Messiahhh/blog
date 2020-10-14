b7240ce36cac5758269e9d305a91b98013dc289d
akaracheng(程浚哲) <akaracheng@tencent.com>
2020-10-14 11:27:42 +0800

    fix: 修复构建问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -86,7 +86,7 @@ module.exports = {
                 'linux',
                 'websocket',
                 'php',
-                '其他'
+                '其它'
             ],
             '/mianshi/': [
                 '',
